0669478ef9903fd2c4534727ee136e95da2d0595
ciro brachetta <cirobrachetta@gmail.com>
2025-04-11 16:10:14 -0300

    terminado

---
+++ poo/actividad-1.js
@@ -0,0 +1,22 @@
+objeto: rociador
+
+/*caracteristicas o atributos:
+Nombre              | Tipo de Dato      | const/var                | Rango de valores posibles  | F(c)
+    -capacidad en ml| float             | const                    | 750.0                      | none
+    -% de carga     | double            | var                      | 0-100                      | rociar, cargar, vaciar
+
+    comportamiento:
+Nombre              | Parametros/Tipo                        | Retorno/tipo
+    -rociar         | none                                   | bool
+    -cargar         | %:double                               | %:double
+    -vaciar         | none                                   | none
+
+
+    Revision:
+        (lleno, vacio)
+        -gatillo
+        -tapa
+        -valvula
+        -tapar 
+        -destapar
+*/
